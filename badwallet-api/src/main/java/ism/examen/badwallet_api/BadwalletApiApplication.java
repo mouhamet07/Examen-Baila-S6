547a378c2f347e181ecd5e55875f30a721e6435c
mouhamet07 <mouhamethiam7@gmail.com>
2026-06-29 09:10:21 +0000

    init

---
+++ badwallet-api/src/main/java/ism/examen/badwallet_api/BadwalletApiApplication.java
@@ -0,0 +1,13 @@
+package ism.examen.badwallet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadwalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadwalletApiApplication.class, args);
+	}
+
+}
