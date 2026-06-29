ff25ee51c008d41d6eae79283931f47f0b1ee510
mouhamet07 <mouhamethiam7@gmail.com>
2026-06-29 09:51:45 +0000

    implementation de "POST {{walletHost}}/api/wallets"

--- badwallet-api/src/main/java/ism/examen/badwallet_api/client/web/controller/WalletController.java
+++ badwallet-api/src/main/java/ism/examen/badwallet_api/client/web/controller/WalletController.java
@@ -1,9 +1,11 @@
-package ism.examen.badwallet_api.wallet.client.web.controller;
+package ism.examen.badwallet_api.client.web.controller;
 
+import ism.examen.badwallet_api.client.web.dto.CreateWalletRequest;
 import ism.examen.badwallet_api.shared.response.ApiResponse;
 import ism.examen.badwallet_api.wallet.service.WalletService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,5 +25,11 @@ public class WalletController {
     ) {
         walletService.seedWallets(numWallets, eventsPerWallet);
         return ResponseEntity.ok(new ApiResponse(numWallets + " wallets generated successfully."));
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse> createWallet(@RequestBody CreateWalletRequest request) {
+        walletService.createWallet(request);
+        return ResponseEntity.ok(new ApiResponse("Wallet created successfully."));
     }
 }
--- badwallet-api/src/main/java/ism/examen/badwallet_api/wallet/service/WalletService.java
+++ badwallet-api/src/main/java/ism/examen/badwallet_api/wallet/service/WalletService.java
@@ -1,6 +1,10 @@
 package ism.examen.badwallet_api.wallet.service;
 
+import ism.examen.badwallet_api.client.web.dto.CreateWalletRequest;
+
 public interface WalletService {
 
     void seedWallets(int numWallets, int eventsPerWallet);
+
+    void createWallet(CreateWalletRequest request);
 }
